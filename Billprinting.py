9892377b59b1eed2696427585bafa49af8ac58c6
Sameer-Tank <tanksameer024@gmail.com>
2024-05-31 14:24:09 +0530

    Add files via upload

---
+++ Billprinting.py
@@ -0,0 +1,18 @@
+while True:
+    Name=input("Enter Customer Name:")
+    total=0
+    while True:
+        print("Enter Amount And Quentity")
+        amount=float(input("Enter Amount:"))
+        quentity=float(input("Enter Quentity:"))
+        total += amount*quentity
+        repet=input("Do you want to add more items?(Yes/No)")
+        if repet=="no:" or repet=="no":
+            break
+    print("Name",Name)
+    print("Amount to be paid",total)
+    print("***** Happy Shopping *****")
+    print("***** Visit Us Again *****")
+    repet1=input("Do you want to go to next customer?(Yes/No)")
+    if repet1=="no" or repet1=="No":
+        break
